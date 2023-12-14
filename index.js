a57a78290d2111a27d4881887dc617a5b83ee7b5
rose <rooserwl@gmail.com>
2023-12-14 21:02:29 +0545

    khalti server continue

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ import http from 'http'
 import dotenv from 'dotenv'
 import router from './routes/index.js';
 import carRouter from './routes/carroute.js';
+import axios from 'axios';
 //Initialization
 dotenv.config();
-const {MONGO_PROD,PORT}=process.env;
+const {MONGO_PROD,PORT,KHALTI_SECRET_KEY}=process.env;
 const app=express()
 app.use(express.json())
 app.use(cors('*'))
@@ -16,7 +17,24 @@ app.get('/',(req,res)=>{
 })
 app.use('/',router)
 app.use('/car',carRouter)
-
+app.post('/khalti-api',async(req,res)=>{
+    const payload=req.body;
+    const response=await axios.post('https://a.khalti.com/api/v2/epayment/initiate/',payload,{
+       headers:{ Authorization: `KEY ${KHALTI_SECRET_KEY}`}
+    })
+    if(response){
+        res.json({
+            success:true,
+            data:response?.data
+        })
+    }else{
+        res.json({
+            success:false,
+            message:'Something Went Wrong'
+        })
+    }
+    console.log(response)
+})
 //Connect DataBase
 mongoose.connect(MONGO_PROD)
 .then(()=>{
